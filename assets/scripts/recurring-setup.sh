10e054c92d291bd54b5f3bb2de3b849a499fe095
Justin Moon <mail@justinmoon.com>
2019-06-20 16:28:36 -0500

    chmod +x scripts; no aliases in scripts

--- assets/scripts/recurring-setup.sh
+++ assets/scripts/recurring-setup.sh
@@ -1,1 +1,1 @@
-lnd
+/workspace/lnd-linux-386-v0.6.1-beta/lnd --lnddir=/workspace/lnd/.lnd --datadir=/workspace/lnd --bitcoin.active --bitcoin.testnet --debuglevel=info --bitcoin.node=neutrino --neutrino.addpeer=btcd-testnet.lightning.computer
