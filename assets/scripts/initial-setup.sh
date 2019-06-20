c886c75417b4a00595e914617d9acfa5c2a2dfda
Justin Moon <mail@justinmoon.com>
2019-06-20 16:41:17 -0500

    Fix fuckups

--- assets/scripts/initial-setup.sh
+++ assets/scripts/initial-setup.sh
@@ -1,8 +1,7 @@
-
 # Install lnd
-RUN wget https://github.com/lightningnetwork/lnd/releases/download/v0.6.1-beta/lnd-linux-386-v0.6.1-beta.tar.gz
-RUN tar -xzf ./lnd-linux-386-v0.6.1-beta.tar.gz
-mv lnd-linux-386-v0.6.1-beta.tar.gz /workspace
+cd /workspace
+wget https://github.com/lightningnetwork/lnd/releases/download/v0.6.1-beta/lnd-linux-386-v0.6.1-beta.tar.gz
+tar -xzf ./lnd-linux-386-v0.6.1-beta.tar.gz
 
 # Install python libs
 pip3 install lnd_grpc
