19b88dc510ff1fcbeb9db4b3da3de5ae547e2741
Justin Moon <mail@justinmoon.com>
2019-06-20 17:43:02 -0500

    Ignore another port, symlink data dir

--- assets/scripts/recurring-setup.sh
+++ assets/scripts/recurring-setup.sh
@@ -1,1 +1,5 @@
+# Symlink datadir to homedir
+ln -s /workspace/lnd/.lnd /home/gitpod/.lnd
+
+# Run LND
 /workspace/lnd-linux-386-v0.6.1-beta/lnd --lnddir=/workspace/lnd/.lnd --datadir=/workspace/lnd --bitcoin.active --bitcoin.testnet --debuglevel=info --bitcoin.node=neutrino --neutrino.addpeer=btcd-testnet.lightning.computer
