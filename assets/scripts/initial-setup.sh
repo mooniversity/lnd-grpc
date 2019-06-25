04956f2a8733317648b848a468c31d30582a3d12
willcl-ark <will8clark@gmail.com>
2019-06-25 20:27:04 +0100

    host binaries on github
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- assets/scripts/initial-setup.sh
+++ assets/scripts/initial-setup.sh
@@ -2,6 +2,6 @@
 
 # Install lnd
 mkdir /workspace/bin && cd /workspace/bin
-wget -O lnd-linux-386-v0.7.0-beta-rc2.tar.gz https://www.dropbox.com/s/pfu5myums1x4h88/lnd-linux-386-v0.7.0-beta-rc2.tar.gz?dl=0
+wget -O lnd-linux-386-v0.7.0-beta-rc2.tar.gz https://github.com/mooniversity/lnd-grpc/releases/download/1.1/lnd-linux-386-v0.7.0-beta-rc2.tar.gz
 tar -xzf lnd-linux-386-${LND_VERSION}.tar.gz && rm lnd-linux-386-${LND_VERSION}.tar.gz
 
