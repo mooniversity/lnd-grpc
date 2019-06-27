34dc2e82202564c12c9d94ec524a78a6334e953d
willcl-ark <will8clark@gmail.com>
2019-06-27 09:19:50 +0100

    Fix
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- assets/scripts/initial-setup.sh
+++ assets/scripts/initial-setup.sh
@@ -6,16 +6,17 @@ mkdir -p /workspace/lnd/.lnd
 mkdir -p /workspace/lnd/.lnd2
 mkdir -p /workspace/bitcoin/.bitcoin
 
+cd /workspace/bin
 
 # Install lnd
-wget -O /workspace/bin/lnd-linux-386-v0.7.0-beta-rc2.tar.gz https://github.com/lightningnetwork/lnd/releases/download/v0.7.0-beta-rc2/lnd-linux-amd64-v0.7.0-beta-rc2.tar.gz
-tar -xzf /workspace/bin/lnd-linux-386-v0.7.0-beta-rc2.tar.gz  && rm /workspace/bin/lnd-linux-386-v0.7.0-beta-rc2.tar.gz
+wget -O lnd-linux-386-v0.7.0-beta-rc2.tar.gz https://github.com/lightningnetwork/lnd/releases/download/v0.7.0-beta-rc2/lnd-linux-amd64-v0.7.0-beta-rc2.tar.gz
+tar -xzf lnd-linux-386-v0.7.0-beta-rc2.tar.gz  && rm lnd-linux-386-v0.7.0-beta-rc2.tar.gz
 
 # Install bitcoind
-wget -O /workspace/bin/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-0.18.0/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
-tar -xzf /workspace/bin/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz && rm /workspace/bin/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
+wget -O bitcoin-0.18.0-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-0.18.0/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
+tar -xzf bitcoin-0.18.0-x86_64-linux-gnu.tar.gz && rm bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
 
 # Copy conf files
-wget -O /workspace/bitcoin/.bitcoin/bitcoin.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/bitcoin.conf
-wget -O /workspace/bitcoin/.lnd/lnd.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/lnd1.conf
-wget -O /workspace/bitcoin/.lnd2/lnd.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/lnd2.conf
+cd /workspace/bitcoin/.bitcoin && wget -O bitcoin.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/bitcoin.conf
+cd /workspace/lnd/.lnd && wget -O lnd.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/lnd1.conf
+cd /workspace/lnd/.lnd2 && wget -O lnd.conf https://github.com/willcl-ark/lnd-grpc/blob/regtest/lnd2.conf
