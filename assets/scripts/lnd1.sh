35b7c3ff7f5d51a77af94b62fe847d44c49cac23
willcl-ark <will8clark@gmail.com>
2019-06-27 10:46:09 +0100

    Final fixes for regtest mode
    Signed-off-by: willcl-ark <will8clark@gmail.com>

---
+++ assets/scripts/lnd1.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/workspace/bin/lnd-linux-amd64-v0.7.0-beta-rc2/lnd --lnddir=/workspace/lnd/.lnd
--- assets/scripts/recurring-setup.sh
+++ assets/scripts/recurring-setup.sh
@@ -1,2 +1,5 @@
 # Symlink datadir to homedir
 ln -s /workspace/lnd/.lnd /home/gitpod/.lnd
+
+# Start bitcoind
+/workspace/bin/bitcoin-0.18.0/bin/bitcoind -datadir=/workspace/bitcoin/.bitcoin
